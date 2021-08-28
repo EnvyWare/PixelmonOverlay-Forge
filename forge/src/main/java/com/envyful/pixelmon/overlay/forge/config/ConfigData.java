c4dd9634a26db00dd36d7d06f87a8dc27ea1c855
danorris709 <dnorris443@gmail.com>
2021-08-28 22:39:42 +0100

    fix(forge): add missing annotation

--- forge/src/main/java/com/envyful/pixelmon/overlay/forge/config/ConfigData.java
+++ forge/src/main/java/com/envyful/pixelmon/overlay/forge/config/ConfigData.java
@@ -1,5 +1,8 @@
 package com.envyful.pixelmon.overlay.forge.config;
 
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+@ConfigSerializable
 public interface ConfigData<A> {
 
     A build();
