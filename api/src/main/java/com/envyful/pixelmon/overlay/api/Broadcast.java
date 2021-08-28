90499f9327c8fda16e7b228e066e342ba69f5378
danorris709 <dnorris443@gmail.com>
2021-08-28 21:18:13 +0100

    feat(builder): add Broadcast.Builder interface

--- api/src/main/java/com/envyful/pixelmon/overlay/api/Broadcast.java
+++ api/src/main/java/com/envyful/pixelmon/overlay/api/Broadcast.java
@@ -2,6 +2,9 @@ package com.envyful.pixelmon.overlay.api;
 
 import com.envyful.api.player.EnvyPlayer;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * Representing a Pixelmon overlay to be sent to the users
@@ -32,4 +35,67 @@ public interface Broadcast {
      */
     void sendAll();
 
+    /**
+     *
+     * Represents a Broadcast builder
+     *
+     * @param <T> type that the builder represents
+     */
+    interface Builder<T> {
+
+        /**
+         *
+         * Specifies the duration of the {@link Broadcast}
+         *
+         * @param duration The duration
+         * @param timeUnit the unit of the duration
+         * @return The builder
+         */
+        Builder<T> duration(long duration, TimeUnit timeUnit);
+
+        /**
+         *
+         * Specifies the duration of the {@link Broadcast} in seconds
+         *
+         * @param durationSeconds the dueation in seconds
+         * @return The builder
+         */
+        Builder<T> duration(int durationSeconds);
+
+        /**
+         *
+         * Specifies the lines of text of the {@link Broadcast}
+         *
+         * @param lines The lines of the broadcast
+         * @return The builder
+         */
+        Builder<T> lines(String... lines);
+
+        /**
+         *
+         * Specifies the lines of text of the {@link Broadcast}
+         *
+         * @param lines The lines of the broadcast
+         * @return The builder
+         */
+        Builder<T> lines(List<String> lines);
+
+        /**
+         *
+         * Specifies the display attributes of the image on the {@link Broadcast}
+         *
+         * @param display The display details
+         * @return The builder
+         */
+        Builder<T> display(T display);
+
+        /**
+         *
+         * Builds the {@link Broadcast} from the provided info
+         *
+         * @return The built {@link Broadcast}
+         */
+        Broadcast build();
+
+    }
 }
