d534160418ef566f7c38af6f8ec09f3f0156dac3
danorris709 <dnorris443@gmail.com>
2021-08-28 22:14:47 +0100

    feat(forge): implement PixelmonDisplay in factory

--- forge/src/main/java/com/envyful/pixelmon/overlay/forge/impl/BroadcastPlatformForge.java
+++ forge/src/main/java/com/envyful/pixelmon/overlay/forge/impl/BroadcastPlatformForge.java
@@ -6,8 +6,8 @@ import com.envyful.pixelmon.overlay.api.BroadcastPlatformFactory;
 import com.envyful.pixelmon.overlay.forge.impl.broadcast.ItemBroadcast;
 import com.envyful.pixelmon.overlay.forge.impl.broadcast.PixelmonBroadcast;
 import com.envyful.pixelmon.overlay.forge.impl.broadcast.TextBroadcast;
+import com.envyful.pixelmon.overlay.forge.impl.broadcast.pixelmon.PixelmonDisplay;
 import com.pixelmonmod.pixelmon.api.overlay.notice.EnumOverlayLayout;
-import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
 import net.minecraft.item.ItemStack;
 
 public class BroadcastPlatformForge implements BroadcastPlatformFactory {
@@ -19,8 +19,8 @@ public class BroadcastPlatformForge implements BroadcastPlatformFactory {
     public <A> Broadcast.Builder<A> builder(A a) {
         if (a instanceof ItemStack) {
             return (Broadcast.Builder<A>) new ItemBroadcast.Builder().display((ItemStack) a);
-        } else if (a instanceof PokemonSpec) {
-            return (Broadcast.Builder<A>) new PixelmonBroadcast.Builder().display((PokemonSpec) a);
+        } else if (a instanceof PixelmonDisplay) {
+            return (Broadcast.Builder<A>) new PixelmonBroadcast.Builder().display((PixelmonDisplay) a);
         } else if (a instanceof String) {
             return (Broadcast.Builder<A>) new TextBroadcast.Builder().display((String) a);
         } else {
