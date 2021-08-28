a14df5621b53265f58e38e3b18e2c4981443d2ef
danorris709 <dnorris443@gmail.com>
2021-08-28 22:11:55 +0100

    feat(forge): add load config

--- forge/src/main/java/com/envyful/pixelmon/overlay/forge/PixelmonOverlayForge.java
+++ forge/src/main/java/com/envyful/pixelmon/overlay/forge/PixelmonOverlayForge.java
@@ -1,14 +1,18 @@
 package com.envyful.pixelmon.overlay.forge;
 
+import com.envyful.api.config.yaml.YamlConfigFactory;
 import com.envyful.api.forge.command.ForgeCommandFactory;
 import com.envyful.api.forge.concurrency.ForgeTaskBuilder;
 import com.envyful.api.forge.player.ForgePlayerManager;
 import com.envyful.pixelmon.overlay.api.BroadcastFactory;
+import com.envyful.pixelmon.overlay.forge.config.PixelmonOverlayConfig;
 import com.envyful.pixelmon.overlay.forge.impl.BroadcastPlatformForge;
 import com.envyful.pixelmon.overlay.forge.task.ClearTask;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+import java.io.IOException;
 
 @Mod(
         modid = "pixelmonoverlay",
@@ -21,6 +25,8 @@ public class PixelmonOverlayForge {
 
     private static PixelmonOverlayForge instance;
 
+    private PixelmonOverlayConfig config;
+
     private ForgePlayerManager playerManager = new ForgePlayerManager();
     private ForgeCommandFactory commandFactory = new ForgeCommandFactory();
 
@@ -30,12 +36,22 @@ public class PixelmonOverlayForge {
 
         BroadcastFactory.setPlatformFactory(new BroadcastPlatformForge());
 
+        this.loadConfig();
+
         new ForgeTaskBuilder()
                 .async(true)
                 .delay(10L)
                 .interval(10L)
                 .task(new ClearTask())
                 .start();
+    }
+
+    private void loadConfig() {
+        try {
+            this.config = YamlConfigFactory.getInstance(PixelmonOverlayConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Mod.EventHandler
