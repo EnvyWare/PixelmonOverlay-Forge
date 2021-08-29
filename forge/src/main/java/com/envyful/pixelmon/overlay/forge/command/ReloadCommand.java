03cffdf989d757bcf1f8bc3e68b42ec18c5920ae
danorris709 <dnorris443@gmail.com>
2021-08-29 09:28:33 +0100

    feat(forge): add reload command

--- forge/src/main/java/com/envyful/pixelmon/overlay/forge/command/ReloadCommand.java
+++ forge/src/main/java/com/envyful/pixelmon/overlay/forge/command/ReloadCommand.java
@@ -1,28 +1,28 @@
 package com.envyful.pixelmon.overlay.forge.command;
 
+import com.envyful.api.command.annotate.Child;
 import com.envyful.api.command.annotate.Command;
-import com.envyful.api.command.annotate.SubCommands;
+import com.envyful.api.command.annotate.Permissible;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.pixelmon.overlay.forge.PixelmonOverlayForge;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.TextComponentString;
 
 @Command(
-        value = "overlay",
-        description = "Root command",
-        aliases = {
-                "pixelmonoverlay"
-        }
+        value = "reload",
+        description = "Reload command"
 )
-@SubCommands(
-        ReloadCommand.class
-)
-public class OverlayCommand {
+@Permissible("pixelmon.overlays.command.reload")
+@Child
+public class ReloadCommand {
 
     @CommandProcessor
     public void onCommand(@Sender ICommandSender sender, String[] args) {
+        PixelmonOverlayForge.getInstance().loadConfig();
         sender.sendMessage(new TextComponentString(UtilChatColour.translateColourCodes('&',
-                "&e&l(!) &e/overlay <reload|toggle>")));
+                "&e&l(!) &eReloaded config")));
+
     }
 }
