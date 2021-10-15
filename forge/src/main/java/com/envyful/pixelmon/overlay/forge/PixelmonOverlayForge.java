408516228fe6d283111c8f5ae334abd26619baf7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-15 17:45:47 +0100

    chore(gradle): version bump

--- forge/src/main/java/com/envyful/pixelmon/overlay/forge/PixelmonOverlayForge.java
+++ forge/src/main/java/com/envyful/pixelmon/overlay/forge/PixelmonOverlayForge.java
@@ -24,7 +24,7 @@ import java.io.IOException;
         acceptableRemoteVersions = "*"
 )
 public class PixelmonOverlayForge {
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.6.0";
 
     private static PixelmonOverlayForge instance;
 
